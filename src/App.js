a430c0c5cadaa32bf2d6da4d6abed18c2c6a689e
CommandMaker <command.maker83@gmail.com>
2021-02-24 23:29:20 +0100

    Chapitre 6, les formulaires (presque fini)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import './App.css';
 /* Import de nos composants */
-import Clock from './components/Clock';
-import Incrementer from './components/Incrementer';
 import HomeComponent from './components/HomeComponent';
-import ManualIncrementer from './components/ManualIncrementer';
-
-Incrementer.defaultProps = {
-  value: 0,
-  step: 1
-}
 
 export default () => {
   return (
       <div className="container">
         <HomeComponent>Command_maker</HomeComponent>
-        <Clock/>
-        <Incrementer start={20} step={10}/>
-        <Incrementer start={10}/>
-        <ManualIncrementer/>
       </div>
   )
 }
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,69 @@
 import React from 'react';
 
 export default class HomeComponent extends React.Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            nom: 'Jean',
+            textarea: 'Mon contenu',
+            selected: 'deuxieme',
+            checked: false
+        }
+
+        this.handleChange.bind(this);
+    }
+
+    handleChange (e) {
+        console.log(e);
+        this.setState({nom: e.target.value});
+    }
+
     render () {
         return (
-            <h1>{this.props.children}</h1>
+            // <>
+            //     <label htmlFor="nom" className="label">Mon nom</label>
+            //     <input type="text" id="nom" name="nom" value={this.state.nom} onChange={e => this.handleChange(e)} className="input"/>
+            //     <br/>
+            //     <br/>
+            //     <label htmlFor="textarea" className="label">Mon textarea</label>
+            //     <textarea className="textarea" id="textarea" value={this.state.textarea} onChange={event => this.setState({textarea: event.target.value})}/>
+            //
+            //     <br/><br/>
+            //     <div className="select">
+            //         <select value={this.state.selected} onChange={event => this.setState({selected: event.target.value})}>
+            //             <option value="ok">OK</option>
+            //             <option value="deuxieme">Deuxième option du select</option>
+            //         </select>
+            //     </div>
+            //     <br/><br/>
+            //
+            //     <label className="checkbox">
+            //         <input type="checkbox" checked={this.state.checked} onChange={event => this.setState({checked: event.target.checked})}/>
+            //         Ma checkbox
+            //     </label>
+            //
+            //     <br/><br/>
+            //
+            //     {this.state.checked ? <div className="checkbox-message">Un message affiché si la checkbox est cochée</div> : null}
+            // </>
+
+            <>
+                <h1 className="title is-3">Formulaire d'inscription</h1>
+                <div className="field">
+                    <label htmlFor="name" className="label">Nom :</label>
+                    <div className="control has-icons-left">
+                        <input type="text" className="input" id="name"/>
+                        <span className="icon is-small is-left">
+                            <i className="fas fa-address-card"/>
+                        </span>
+                    </div>
+                </div>
+                
+                <div className="field">
+                    <label htmlFor="" className="label" id=""></label>
+                </div>
+            </>
         );
     }
 }
